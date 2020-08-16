6c4c0d668538ba611cfb2609622b69ef9e9a2261
muhammadjewel <msuratov@gmail.com>
2020-08-16 18:27:19 +0500

    Show/close modal on button click

--- js/main.js
+++ js/main.js
@@ -7,7 +7,40 @@ var ready = function (cb) {
   : cb();
 };
 
-
+// Usage
 ready(function() {
-  // Project code
+  
+  // Open/close modal
+  var elModalOpenTrigger = document.querySelector('.js-modal-open-trigger');
+  var elModal = document.querySelector('.modal');
+  var elModalClose = elModal.querySelector('.modal__close');
+
+  var closeModal = function () {
+    elModal.classList.remove('modal--open');
+  };
+  
+  var openModal = function () {
+    elModal.classList.add('modal--open');
+  };
+
+  if (elModalOpenTrigger) {
+    elModalOpenTrigger.addEventListener('click', function (evt) {
+      openModal();
+    });
+  }
+
+  if (elModalClose) {
+    elModalClose.addEventListener('click', function () {
+      closeModal();
+    });
+  }
+
+  if (elModal) {
+    elModal.addEventListener('click', function (evt) {
+      if (evt.target.matches('.modal')) {
+        closeModal();
+      }
+    });
+  }
+  
 });
